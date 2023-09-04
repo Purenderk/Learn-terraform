e50efce8cee00279f51a0f1ee978515281453665
PurenderK <purenderdevops@gmail.com>
2023-09-04 00:13:41 -0700

    LATEST

---
+++ Loops/main.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "timing-11"
+    Terraform = "true"
+    Environment = "DEV"
+  }
+  }
+
+  resource "aws_subnet" "public_subnet"{
+    count = length(var.public_subnet_cidr) #count2
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidr[count.index]
+    availability_zone = var.azs[count.index]
+
+    tags={
+      Name = var.public_subnet_names[count.index]
+    }  
+  }
+
+  resource "aws_subnet" "private_subnet"{
+    for_each = var.private_subnets
+    vpc_id = aws_vpc.main.id
+    cidr_block = each.value.cidr
+    availability_zone = each.value.az
+
+    tags={
+      Name = each.value.Name
+    }  
+  }
